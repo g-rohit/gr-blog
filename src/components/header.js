9e820430be6c0a10cf9fe7e0795c95b337f0a4d4
G Rohit <43009327+g-rohit@users.noreply.github.com>
2020-07-23 22:40:27 +0530

    added navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,62 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Layout from "../components/layout"
+import { useState } from 'react';
+// import Layout from "../components/layout"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+   NavbarText
+} from 'reactstrap';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+
+
+const Header = ({ siteTitle }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <header>
+
+
+
+      <Navbar color="light" className="bg-navyblue" expand="sm" fixed='top'>
+      <div className="container">
+        <Link className="navbar-brand" to="/">{ siteTitle }</Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/" className="nav-link">Home</Link>
+              </NavItem>
+              <NavItem>
+              <Link to="/tags/" className="nav-link">Tags</Link>
+ 
+              </NavItem>
+              <NavItem>
+              <Link to="/team/" className="nav-link">Teams</Link>
+              </NavItem>
+              <NavItem>
+              <Link to="/about/" className="nav-link">About</Link>
+              </NavItem>
+        </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
+        </Collapse>
+        </div>
+      </Navbar>
+     
+    </header>
+  );
+}
+
+ 
+ 
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
---
+++ src/pages/tags.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const TagsPage = () => (
+  <Layout>
+    <SEO title="Tags" />
+    <h1>Below are the different tags available on this blog</h1>
+    </Layout>
+)
+
+export default TagsPage
